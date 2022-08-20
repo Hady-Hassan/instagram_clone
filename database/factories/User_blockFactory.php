2ae9f32adeb5e8547c2cf44bdeeb6b616f7a6c52
Hady-Hassan <90941057+Hady-Hassan@users.noreply.github.com>
2022-08-20 02:33:20 +0200

    relations factory

---
+++ database/factories/User_blockFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class User_blockFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => \App\Models\User::inRandomOrder()->first()->id,
+            'target_id' => \App\Models\User::inRandomOrder()->first()->id,
+            
+        ];
+    }
+}
