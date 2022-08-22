bc399cc8e3586bb02f3422b2e6f2084110a131da
Ahmed Haggag <AhmedHaggag720@gmail.com>
2022-08-22 23:39:33 +0200

    form edit

--- resources/views/pages/EditProfile.blade.php
+++ resources/views/pages/EditProfile.blade.php
@@ -1,11 +1,15 @@
+@extends('layouts.default')
+@section('page_title', 'Edit')
+
+@section('content')
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
     <link rel="stylesheet" href="./stylesheet.css" />
     <link rel="icon" type="image/x-icon" href="/assets/icons8-instagram-color-32.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -27,48 +31,46 @@
             </div>
 
             <div class="col-md-9 px-5">
-                <form class="bg-white border py-4 px-5" method="POST">
-
+                <form class="bg-white border py-4 px-5" method="POST" action="{{Route('users.update',['id'=>$user->id])}}" enctype="multipart/form-data">
+                    @method("PUT")
+                    @csrf
                     <div class="mb-4">
                         <div style="display: inline-block;">
-                            <img class="rounded-circle z-depth-2 mr-2" alt="100x100"
-                                style="max-width: 60px; display: inline-block;"
-                                src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" data-holder-rendered="true">
+                            <img class="rounded-circle z-depth-2 mr-2" width="50" height="50" alt="100x100" src="{{ auth()->user()->avatar != null ? \Storage::url(auth()->user()->avatar) : asset('temp/assets/no pic.jpg')}}" data-holder-rendered="true">
 
                         </div>
                         <div class="px-5" style="display: inline-block;">
-                            <h5 type="text" class="text-left " style="display: inline-block;"> Haggag</h5>
+                            <h5 type="text" class="text-left " style="display: inline-block;"> {{ $user->fullname }}</h5>
                         </div>
 
 
                     </div>
 
                     <div class="mb-3 row">
-                        <label for="fname" name="fname" class="col-sm-2 col-form-label  ">Name</label>
+                        <label for="fname" class="col-sm-2 col-form-label  ">Name</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control " id="fname" value=" Ahmed Haggag">
+                            <input type="text" class="form-control " id="fullname" name="fullname" value="{{ $user->fullname }}">
                         </div>
                     </div>
 
                     <div class="mb-3 row">
                         <label for="username" class="col-sm-2 col-form-label  ">UserName</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="username" id="username" value="Hegooo">
+                            <input type="text" class="form-control" name="username" id="username" value="{{ $user->username }}">
                         </div>
                     </div>
 
                     <div class="mb-3 row">
                         <label for="website" class="col-sm-2 col-form-label  ">Website</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="website" name="wesbite">
+                            <input type="text" class="form-control" id="website" name="website" value="{{ $user->website }}">
                         </div>
                     </div>
 
                     <div class="mb-3 row">
                         <label for="bio" class="col-sm-2 col-form-label  ">Bio</label>
                         <div class="col-sm-10">
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
-                                name="bio"></textarea>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="bio">{{ $user->bio }}</textarea>
                         </div>
                     </div>
 
@@ -77,22 +79,34 @@
                     <div class="mb-3 row">
                         <label for="phonenumber" class="col-sm-2 col-form-label  ">Phone Number</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="phonenumber" name="phonenumber"
-                                value="01288339181">
+                            <input type="text" class="form-control" id="phonenumber" name="phonenumber" value="{{ $user->phone }}">
                         </div>
                     </div>
 
                     <div class="mb-3 row">
                         <label for="email" class="col-sm-2 col-form-label  ">Gender</label>
                         <div class="col-sm-10">
-                            <select class="form-select form-select-sm" name="gender"
-                                aria-label=".form-select-sm example">
-                                <option selected disabled>Open this select menu</option>
-                                <option value="1">Male</option>
-                                <option value="2">Female</option>
+                            <select class="form-select form-select-sm" name="gender" aria-label=".form-select-sm example">
+
+                                <option disabled>Open this select menu</option>
+                                <option {{ $user->gender === 'm' ?  "selected" : " "}} value="1">Male</option>
+                                <option {{ $user->gender === 'f' ?  "selected" : " "}} value="2">Female</option>
                             </select>
                         </div>
                     </div>
+
+                    <div class="mb-3 row">
+                        <!-- <label for="phonenumber" class="col-sm-2 col-form-label  ">Phone Number</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="phonenumber" name="phonenumber" value="{{ $user->phone }}">
+                        </div> -->
+                        <label class="form-label col-sm-2 col-form-label" for="customFile">Change Image</label>
+                        <div class="col-sm-10">
+                        <input type="file" class="form-control" id="customFile" name="avatar"/>
+                        </div>
+                    </div>
+
+
 
                     <center>
                         <button class="btn btn-primary fw-bold bg-gradient " href="#" type="submit">
@@ -107,8 +121,7 @@
                         <label for="email" class="col-sm-2 col-form-label  ">Email</label>
 
                         <div class="col-sm-8">
-                            <input type="text" class="form-control" id="email" name="email"
-                                value="AhmedHaggag720@gmail.com">
+                            <input type="text" class="form-control" id="email" name="email" value="AhmedHaggag720@gmail.com">
                         </div>
 
                         <div class="col-sm-2">
@@ -122,14 +135,13 @@
 
 
                 <form class="bg-white border py-4 px-5" method="POST">
-                    <div class="mb-3 row" >
+                    <div class="mb-3 row">
                         <label for="email" class="col-sm-2 col-form-label  ">Email</label>
 
                         <div class="col-sm-8">
-                            <input type="text" class="form-control" id="email" name="email"
-                                value="AhmedHaggag720@gmail.com">
+                            <input type="text" class="form-control" id="email" name="email" value="AhmedHaggag720@gmail.com">
                         </div>
-                        <div class="col-sm-2 mt-auto mb-auto" >
+                        <div class="col-sm-2 mt-auto mb-auto">
                             <span class="badge bg-dark">Verified</span>
                         </div>
 
@@ -144,3 +156,4 @@
 </body>
 
 </html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;  
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\profileController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,18 +33,22 @@ Route::group(['middleware'=>'auth'],function(){
 
 
 
-    Route::get('/user',[UserController::class,'index'])->name('users.index');
-    Route::get('/user/create',[UserController::class,'create'])->name('users.create')->middleware(['auth']);
-    Route::post('/user',[UserController::class,'store'])->name('users.store')->middleware(['auth']);
+    // Route::get('/user',[UserController::class,'index'])->name('users.index');
+    // Route::get('/user/create',[UserController::class,'create'])->name('users.create')->middleware(['auth']);
+    // Route::post('/user',[UserController::class,'store'])->name('users.store')->middleware(['auth']);
     
-    Route::get('/user/{user}',[UserController::class,'show'])->name('users.show');
-    Route::get('/user/{user}/edit',[UserController::class,'edit'])->where('user', '[0-9]+')->name('users.edit')->middleware(['auth']);
-    Route::put('/user/{user}',[UserController::class,'update'])->where('user', '[0-9]+')->name('users.update')->middleware(['auth']);
-    Route::delete('/user/{user}',[UserController::class,'destroy'])->where('user', '[0-9]+')->name('users.destroy')->middleware(['auth']);
+    // Route::get('/user/{user}',[UserController::class,'show'])->name('users.show');
+    // Route::get('/user/{user}/edit',[UserController::class,'edit'])->where('user', '[0-9]+')->name('users.edit')->middleware(['auth']);
+    // Route::put('/user/{user}',[UserController::class,'update'])->where('user', '[0-9]+')->name('users.update')->middleware(['auth']);
+    // Route::delete('/user/{user}',[UserController::class,'destroy'])->where('user', '[0-9]+')->name('users.destroy')->middleware(['auth']);
     
 
     Route::get('/user/{user}/post/{post}',[PostController::class,'show'])->where('post', '[0-9]+')->name('post.show');
 
+
+    //Route::get('/editpage',[profileController::class,'index'])->name('users.index');
+    Route::get('/users/edit', [profileController::class, 'edit'])->where('id', '[0-9]+')->name('users.edit');
+    Route::put('/users/update', [profileController::class, 'update'])->where('id', '[0-9]+')->name('users.update');
     
 });
 
