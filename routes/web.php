73c532e9f0f684f01164da021c5948b2f1bdd4fa
omarabdelfattah <omar.mero200125@gmail.com>
2022-08-22 23:47:16 +0200

    make comment route

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,11 @@ Route::group(['middleware'=>'auth'],function(){
        //Route::get('/editpage',[profileController::class,'index'])->name('users.index');
        Route::get('/users/edit', [profileController::class, 'edit'])->where('id', '[0-9]+')->name('users.edit');
        Route::put('/users/update', [profileController::class, 'update'])->where('id', '[0-9]+')->name('users.update');
+
+
+    // comments
+    Route::post('/user/{user}/post/{post}',[PostController::class,'makeComment'])->where('post', '[0-9]+')->name('post.make_comment');
+
     
 });
 
