5cafe3cd840e1948fd1dbb6fc9ab899e47e73eda
Ahmed Haggag <AhmedHaggag720@gmail.com>
2022-08-23 01:34:35 +0200

    styleerror

--- resources/views/pages/EditProfile.blade.php
+++ resources/views/pages/EditProfile.blade.php
@@ -2,21 +2,6 @@
 @section('page_title', 'Edit')
 
 @section('content')
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
-    <link rel="stylesheet" href="./stylesheet.css" />
-    <link rel="icon" type="image/x-icon" href="/assets/icons8-instagram-color-32.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Instagram</title>
-</head>
-
-<body>
     <div class="container">
 
         <div class="row">
@@ -153,7 +138,7 @@
         </div>
     </div>
 
-</body>
 
-</html>
+
+
 @endsection
