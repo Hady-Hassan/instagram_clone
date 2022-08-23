9f645624ca03a0700af1fc565382c9a95186e8ff
omarabdelfattah <omar.mero200125@gmail.com>
2022-08-23 16:13:55 +0200

    tags finished

--- resources/views/pages/tag.blade.php
+++ resources/views/pages/tag.blade.php
@@ -13,12 +13,13 @@
     <div class="row">
         @foreach($posts as $post)
         <div class="col-md-4 my-5">
-            <div class="card  my-4 m-auto">
+            <div class="card  tag_card my-4 m-auto">
                 <div class="card-body">
+                    
                     <a href="{{ Route('post.show',['user' =>  $post->user->username , 'post' => $post->id  ]) }}">
                         @if($post->media->first()->type == 'p')
                         <img src="{{ \Storage::url($post->media->first()->Path)   }}" style="max-width:100%"
-                            class="img-responsive" alt="">
+                            class="col-12 w-100 m-auto" alt="">
                         @else
                         <video controls class="col-12 w-100 m-auto" tyle="max-width:100%">
                             <source src="{{ \Storage::url($post->media->first()->Path)   " type="video/mp4">
