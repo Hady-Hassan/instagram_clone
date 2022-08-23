b86e5b1d4ae5e26e697b3bfe432348c4bda1e108
Mohamed Hesham <moohisham2000@gmail.com>
2022-08-24 00:20:26 +0200

    comments working in post page

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -164,64 +164,64 @@
                     <div class="col-1 p-0 ">
                       <!-- Like form -->
                       <form method="POST">
-                      <input type="checkbox" class="heart" id="heart" name="like"/>
-                    <label for="heart" class="opacity-100">
-                      <svg id="heart-svg" viewBox="467 392 58 57" xmlns="http://www.w3.org/2000/svg" >
-                        <g id="Group" fill="none" fill-rule="evenodd" transform="translate(467 392)">
-                          <path d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z" id="heart" fill="#AAB8C2"/>
-                          <circle id="main-circ" fill="#E2264D" opacity="0" cx="29.5" cy="29.5" r="1.5"/>
-                
-                          <g id="heartgroup7" opacity="0" transform="translate(7 6)">
-                            <circle id="heart1" fill="#9CD8C3" cx="2" cy="6" r="2"/>
-                            <circle id="heart2" fill="#8CE8C3" cx="5" cy="2" r="2"/>
-                          </g>
-                
-                          <g id="heartgroup6" opacity="0" transform="translate(0 28)">
-                            <circle id="heart1" fill="#CC8EF5" cx="2" cy="7" r="2"/>
-                            <circle id="heart2" fill="#91D2FA" cx="3" cy="2" r="2"/>
-                          </g>
-                
-                          <g id="heartgroup3" opacity="0" transform="translate(52 28)">
-                            <circle id="heart2" fill="#9CD8C3" cx="2" cy="7" r="2"/>
-                            <circle id="heart1" fill="#8CE8C3" cx="4" cy="2" r="2"/>
-                          </g>
-                
-                          <g id="heartgroup2" opacity="0" transform="translate(44 6)">
-                            <circle id="heart2" fill="#CC8EF5" cx="5" cy="6" r="2"/>
-                            <circle id="heart1" fill="#CC8EF5" cx="2" cy="2" r="2"/>
-                          </g>
-                
-                          <g id="heartgroup5" opacity="0" transform="translate(14 50)">
-                            <circle id="heart1" fill="#91D2FA" cx="6" cy="5" r="2"/>
-                            <circle id="heart2" fill="#91D2FA" cx="2" cy="2" r="2"/>
-                          </g>
-                
-                          <g id="heartgroup4" opacity="0" transform="translate(35 50)">
-                            <circle id="heart1" fill="#F48EA7" cx="6" cy="5" r="2"/>
-                            <circle id="heart2" fill="#F48EA7" cx="2" cy="2" r="2"/>
-                          </g>
-                
-                          <g id="heartgroup1" opacity="0" transform="translate(24)">
-                            <circle id="heart1" fill="#9FC7FA" cx="2.5" cy="3" r="2"/>
-                            <circle id="heart2" fill="#9FC7FA" cx="7.5" cy="2" r="2"/>
-                          </g>
-                        </g>
-                      </svg>
-                    </label>
-                  </form>
+              <input type="checkbox" class="heart" {{ $post->isLiked() ? "checked" : " " }} id="heart" name="heart_{{ $post->id }}" value="heart_{{ $post->id }}"/>
+              <label  for="heart_{{ $post->id }}" data-action="like" data-postid="{{ $post->id }}" class="opacity-100" onclick="toggle(this)">
+              <svg id="heart-svg" viewBox="467 392 58 57" xmlns="http://www.w3.org/2000/svg">
+                <g id="Group" fill="none" fill-rule="evenodd" transform="translate(467 392)">
+                  <path d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z" id="heart" fill="#AAB8C2"/>
+                  <circle id="main-circ" fill="#E2264D" opacity="0" cx="29.5" cy="29.5" r="1.5"/>
+        
+                  <g id="heartgroup7" opacity="0" transform="translate(7 6)">
+                    <circle id="heart1" fill="#9CD8C3" cx="2" cy="6" r="2"/>
+                    <circle id="heart2" fill="#8CE8C3" cx="5" cy="2" r="2"/>
+                  </g>
+        
+                  <g id="heartgroup6" opacity="0" transform="translate(0 28)">
+                    <circle id="heart1" fill="#CC8EF5" cx="2" cy="7" r="2"/>
+                    <circle id="heart2" fill="#91D2FA" cx="3" cy="2" r="2"/>
+                  </g>
+        
+                  <g id="heartgroup3" opacity="0" transform="translate(52 28)">
+                    <circle id="heart2" fill="#9CD8C3" cx="2" cy="7" r="2"/>
+                    <circle id="heart1" fill="#8CE8C3" cx="4" cy="2" r="2"/>
+                  </g>
+        
+                  <g id="heartgroup2" opacity="0" transform="translate(44 6)">
+                    <circle id="heart2" fill="#CC8EF5" cx="5" cy="6" r="2"/>
+                    <circle id="heart1" fill="#CC8EF5" cx="2" cy="2" r="2"/>
+                  </g>
+        
+                  <g id="heartgroup5" opacity="0" transform="translate(14 50)">
+                    <circle id="heart1" fill="#91D2FA" cx="6" cy="5" r="2"/>
+                    <circle id="heart2" fill="#91D2FA" cx="2" cy="2" r="2"/>
+                  </g>
+        
+                  <g id="heartgroup4" opacity="0" transform="translate(35 50)">
+                    <circle id="heart1" fill="#F48EA7" cx="6" cy="5" r="2"/>
+                    <circle id="heart2" fill="#F48EA7" cx="2" cy="2" r="2"/>
+                  </g>
+        
+                  <g id="heartgroup1" opacity="0" transform="translate(24)">
+                    <circle id="heart1" fill="#9FC7FA" cx="2.5" cy="3" r="2"/>
+                    <circle id="heart2" fill="#9FC7FA" cx="7.5" cy="2" r="2"/>
+                  </g>
+                </g>
+              </svg>
+            </label>
+          </form>
                    <!-- End Like form -->
                     </div>
                      <!--End Like button -->
                     <div class="col-1 p-0 mt-auto mb-auto">
                       <!-- Save form -->
                       <form method="POST">
-                      <input type="checkbox" class="saved" id="saved" name="saved"/>
-                      <label for="saved" class="opacity-100">
-                        <svg xmlns="http://www.w3.org/2000/svg" id="saved-svg" fill="gray"  class="bi bi-bookmark" viewBox="0 0 16 16">
-                          <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
-                        </svg>
-                      </label>
-                    </form>
+              <input type="checkbox" class="saved" {{ $post->isSaved() ? "checked" : " " }} id="saved" name="save_{{ $post->id }}" value="save_{{ $post->id }}"/>
+              <label for="save_{{ $post->id }}" data-action="save" data-postid="{{ $post->id }}"  class="opacity-100" onclick = "toggle(this)">
+                <svg xmlns="http://www.w3.org/2000/svg" id="saved-svg" fill="gray"  class="bi bi-bookmark" viewBox="0 0 16 16">
+                  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+                </svg>
+              </label>
+            </form>
                     <!-- End save form -->
                   </div>
                   <div style="font-size:16px;font-weight:500;">{{ $post->likes->count() }} likes</div>
@@ -230,19 +230,20 @@
                 </div>
                 <div class="card-footer text-muted container-fluid p-0">
                     <!-- Comment form -->
+                    <form data-postid="{{ $post->id }}" class="comment_post" name="comment_post" id="comment_post_{{ $post->id }}" method="POST" action="{{ Route('post.make_comment',['user' =>  $post->user->username , 'post' => $post->id  ]) }}">
                     <div class="row p-0">
                       <div class="col-10 ">
-                          <textarea cols="45" rows="1" class="form-control" id="comment"  name="comment" placeholder="write your comment.." style="border-radius: 0; font-size: 16px;"></textarea>
+                        <textarea cols="45" rows="1" class="form-control" id="comment"  name="comment" placeholder="write your comment.." style="border-radius: 0; font-size: 16px;"></textarea>
                       </div>
                       <div class="col-2 m-auto">
                         <input type="submit" value="POST" class="btn btn-default" style="color:#3cacfc; font-weight:500;border-radius: 0;">
                       </div>
                     <!-- End Comment Form -->
                   </div>
+                  </form>
                 </div>
               </div>
         </div>
       </div>
     </div>
-
 @endsection
