9aa027233a62ada5cd50089fd7c6fbc91bc3f6a0
omarabdelfattah <omar.mero200125@gmail.com>
2022-08-24 02:10:59 +0200

    show the auth user posts' tag

--- app/Http/Controllers/tagController.php
+++ app/Http/Controllers/tagController.php
@@ -50,7 +50,7 @@ class tagController extends Controller
         $users = auth()->user()->following()->pluck('target_id');
 
         // get the post by id   
-        $posts = Post::whereIn('user_id',$users)->where('caption', 'like', '%#'.$tag.'%')->get();
+        $posts = Post::whereIn('user_id',$users)->orWhere('user_id',auth()->user()->id)->where('caption', 'like', '%#'.$tag.'%')->get();
 
         if($posts->isEmpty()){
             return redirect()->route('home');
