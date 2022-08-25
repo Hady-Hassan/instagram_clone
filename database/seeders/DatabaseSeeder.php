56f7679811f5a88d83d64127b3f6a3220c48601f
S7S-231 <abdelrhman.ahmed54@gmail.com>
2022-08-25 18:20:21 +0200

    my info

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'hadyhassn5@gmail.com',
             'email_verified_at' => now(),
             'password' => bcrypt(1234), // password
-            
+
             'bio'=>'',
             'website'=>'',
             'gender'=>'m',
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'moohisham2000@gmail.com',
             'email_verified_at' => now(),
             'password' => bcrypt(1234), // password
-            
+
             'bio'=>'WUT',
             'website'=>'',
             'gender'=>'m',
@@ -51,12 +51,12 @@ class DatabaseSeeder extends Seeder
             'email' => 'AhmedHaggag720@gmail.com',
             'email_verified_at' => now(),
             'password' => bcrypt(1234), // password
-            
+
             'bio'=>'Just Student',
             'website'=>'www.Ahmed.com',
             'gender'=>'m',
             'phone'=> '01288339181'
-        ]);         
+        ]);
         //Omar Abd El fatah account
         \App\Models\User::create([
             'fullname' =>'Hady Hassan' ,
@@ -64,26 +64,26 @@ class DatabaseSeeder extends Seeder
             'email' => 'hadyhassn5@gmail.com',
             'email_verified_at' => now(),
             'password' => bcrypt(1234), // password
-            
+
             'bio'=>'',
             'website'=>'',
             'gender'=>'m',
             'phone'=> '01284630902'
          ]);
 
-        //Abd alrahman Ahmed account
+        //Abdelrahman Ahmed account
         \App\Models\User::create([
-            'fullname' =>'Hady Hassan' ,
-            'username' =>'Hady_Hassan' ,
-            'email' => 'hadyhassn5@gmail.com',
+            'fullname' =>'Abdelrahman Hussein' ,
+            'username' =>'S7S' ,
+            'email' => 'abdelrhman.ahmed54@gmail.com',
             'email_verified_at' => now(),
             'password' => bcrypt(1234), // password
-            
-            'bio'=>'',
-            'website'=>'',
+
+            'bio'=>'FML',
+            'website'=>'https://yts.mx/',
             'gender'=>'m',
-            'phone'=> '01284630902'
-         ]); 
+            'phone'=> '01111507177'
+         ]);
         \App\Models\User::factory(200)->create();
         \App\Models\Post::factory(2000)->create();
         $this->call(media::class);
